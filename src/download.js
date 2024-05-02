0587ac25ffeef7db4ac1664dfab73cbc6e6c0c9c
Ushaxoxo <nandhiniusha2003@gmail.com>
2024-05-02 19:16:58 +0530

    #14: add choke and unchoke handlers

--- src/download.js
+++ src/download.js
@@ -1,70 +1,104 @@
 'use strict';
 
 const net = require('net');
-const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
+const message = require('./message');
+// 1
+const Pieces = require('./Pieces');
 
 module.exports = torrent => {
   tracker.getPeers(torrent, peers => {
-    peers.forEach(peer => download(peer, torrent));
-
+    // 1
+    const pieces = new Pieces(torrent.info.pieces.length / 20);
+    peers.forEach(peer => download(peer, torrent, pieces));
   });
 };
 
-function download(peer) {
-  const socket = net.Socket();
+// 1
+function download(peer, torrent, pieces) {
+  const socket = new net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
     socket.write(message.buildHandshake(torrent));
   });
-  onWholeMsg(socket, msg => msgHandler(msg, socket));
-
+  // 1
+  const queue = {choked: true, queue: []};
+  onWholeMsg(socket, msg => msgHandler(msg, socket, pieces, queue));
 }
 
 function onWholeMsg(socket, callback) {
-    let savedBuf = Buffer.alloc(0);
-    let handshake = true;
-  
-    socket.on('data', recvBuf => {
-      // msgLen calculates the length of a whole message
-      const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
-      savedBuf = Buffer.concat([savedBuf, recvBuf]);
-  
-      while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
-        callback(savedBuf.slice(0, msgLen()));
-        savedBuf = savedBuf.slice(msgLen());
-        handshake = false;
-      }
-    });
-  }
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
 
-  function msgHandler(msg, socket) {
-    if (isHandshake(msg)) {
-        socket.write(message.buildInterested());
-      } else {
-        const m = message.parse(msg);
-    
-        if (m.id === 0) chokeHandler();
-        if (m.id === 1) unchokeHandler();
-        if (m.id === 4) haveHandler(m.payload);
-        if (m.id === 5) bitfieldHandler(m.payload);
-        if (m.id === 7) pieceHandler(m.payload);
-      }
+  socket.on('data', recvBuf => {
+    // msgLen calculates the length of a whole message
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
     }
-    
-    function chokeHandler() { }
-    
-    function unchokeHandler() { }
-    
-    function haveHandler(payload) { }
-    
-    function bitfieldHandler(payload) {  }
-    
-    function pieceHandler(payload) {  }
-  
+  });
+}
 
-  function isHandshake(msg) {
-    return msg.length === msg.readUInt8(0) + 49 &&
-           msg.toString('utf8', 1) === 'BitTorrent protocol';
+// 1
+function msgHandler(msg, socket, pieces, queue) {
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler(socket);
+    // 1
+    if (m.id === 1) unchokeHandler(socket, pieces, queue);
+    if (m.id === 4) haveHandler(m.payload);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
   }
+}
 
+function isHandshake(msg) {
+  return msg.length === msg.readUInt8(0) + 49 &&
+         msg.toString('utf8', 1) === 'BitTorrent protocol';
+}
+
+function chokeHandler(socket) {
+  socket.end();
+}
+
+// 1
+function unchokeHandler(socket, pieces, queue) {
+  queue.choked = false;
+  // 2
+  requestPiece(socket, pieces, queue);
+}
+
+function haveHandler() {
+  // ...
+}
+
+function bitfieldHandler() {
+  // ...
+}
+
+function pieceHandler() {
+  // ...
+}
+
+//1
+function requestPiece(socket, pieces, queue) {
+  //2
+  if (queue.choked) return null;
+
+  while (queue.queue.length) {
+    const pieceIndex = queue.shift();
+    if (pieces.needed(pieceIndex)) {
+      // need to fix this
+      socket.write(message.buildRequest(pieceIndex));
+      pieces.addRequested(pieceIndex);
+      break;
+    }
+  }
+}
